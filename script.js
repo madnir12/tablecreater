1e5601d359a911490501f2e5981b2eb229cc9cb6
madnir12 <108262050+madnir12@users.noreply.github.com>
2022-08-02 11:56:23 +0500

    table creater

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("form").addEventListener("submit",(event)=>{
+    event.preventDefault()
+
+});
+function tableF(){
+    document.getElementById("insert").innerHTML ="";
+    let input = document.getElementById("input").value;
+    let tableTH = `<tr>
+    <th class="bottomline">Table Of ${input}</th>
+    <th class="bottomline leftline">No:</th>
+
+</tr>`;
+document.getElementById("insert").innerHTML =tableTH;
+    
+    let inputnb = input;
+    for( let index = 1; index <= 10; index++){
+        let nb = inputnb*index;
+        let html = `<tr><td>${input} x ${index} =</td>
+        <td class="leftline">${nb}</td></tr>
+    `;
+    document.getElementById("insert").innerHTML += html;
+    }
+}
