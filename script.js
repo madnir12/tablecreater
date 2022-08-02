fef087d4532c7456720ee3d39ef3fd5c847e459a
madnir12 <108262050+madnir12@users.noreply.github.com>
2022-08-02 23:10:03 +0500

    after adding some more functionality

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
+const normalli = document.getElementById("normalli");
+const customli = document.getElementById("customli");
+const custom = document.getElementById("customC"); 
+
+customli.addEventListener("click",()=>{
+   custom.style.display = "flex";
+    addRemoveClass(customli,normalli)
+});
+normalli.addEventListener("click",()=>{
+    custom.style.display = "none";
+    addRemoveClass(normalli,customli);
+    document.getElementById("start").value = 1;
+    document.getElementById("end").value = 10;
+    tableF();
+
+});
+
+// function for add remove class
+function addRemoveClass(customli,normalli){
+    customli.classList.add("active");
+    normalli.classList.remove("active");
+
+}
+
+// function for solving submit event issue
 document.getElementById("form").addEventListener("submit",(event)=>{
     event.preventDefault()
 
 });
+// function for creating table
 function tableF(){
+    
     document.getElementById("insert").style.display ="block";
     document.getElementById("insert").innerHTML ="";
     let input = document.getElementById("input").value;
+    let start = document.getElementById("start").value;
+    let end = document.getElementById("end").value;
+    if(input >0 && start >0 && end >0){
+    document.getElementById("msg").innerText ="";
     let tableTH = `<tr>
     <th class="bottomline">Table Of ${input}</th>
     <th class="bottomline leftline">No:</th>
@@ -14,11 +45,17 @@ function tableF(){
 document.getElementById("insert").innerHTML =tableTH;
     
     let inputnb = input;
-    for( let index = 1; index <= 10; index++){
+    
+    for( let index = start; index <= end; index++){
         let nb = inputnb*index;
         let html = `<tr><td>${input} x ${index} =</td>
         <td class="leftline">${nb}</td></tr>
     `;
     document.getElementById("insert").innerHTML += html;
     }
+    }else{
+        document.getElementById("msg").innerText = "Please enter all values";
+
+    }
+    
 }
